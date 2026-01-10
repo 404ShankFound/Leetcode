d272aaa940cbbd5e4a992f0d94388e558af09a3b
404ShankFound <shashanknandan21@gmail.com>
2026-01-10 16:12:58 +0530

    File updated at 10/01/2026, 16:12:54

--- 7-ReverseInteger/7-ReverseInteger.c
+++ 7-ReverseInteger/7-ReverseInteger.c
@@ -1,26 +1,21 @@
-// Last updated: 10/01/2026, 15:58:27
+// Last updated: 10/01/2026, 16:12:54
 1#include <limits.h>
-2
-3int reverse(int x) {
-4    int rev = 0;
-5
-6    while (x != 0) {
-7        int digit = x % 10;
-8        x /= 10;
-9
-10        // OVERFLOW CHECK
-11        if (rev > INT_MAX / 10 || 
-12           (rev == INT_MAX / 10 && digit > 7))
-13            return 0;
-14
-15        // UNDERFLOW CHECK
-16        if (rev < INT_MIN / 10 || 
-17           (rev == INT_MIN / 10 && digit < -8))
-18            return 0;
-19
-20        rev = rev * 10 + digit;
-21    }
-22
-23    return rev;
-24}
-25
+2int reverse(int x) {
+3    int rev = 0;
+4    const int MAX_DIV_10 = INT_MAX / 10;
+5    const int MIN_DIV_10 = INT_MIN / 10;
+6
+7    while (x != 0) {
+8        int digit = x % 10;
+9        x /= 10;
+10        // overflow check
+11        if (rev > MAX_DIV_10 || (rev == MAX_DIV_10 && digit > 7))
+12            return 0;
+13        // underflow check
+14        if (rev < MIN_DIV_10 || (rev == MIN_DIV_10 && digit < -8))
+15            return 0;
+16        rev = rev * 10 + digit;
+17    }
+18    return rev;
+19}
+20
