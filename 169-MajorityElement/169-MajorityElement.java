e94f3cacc7deb57020429822baec37a56213d124
404ShankFound <shashanknandan21@gmail.com>
2026-01-21 18:46:26 +0530

    File updated at 21/01/2026, 18:46:15

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,16 +1,17 @@
-// Last updated: 21/01/2026, 18:43:05
+// Last updated: 21/01/2026, 18:46:15
 1class Solution {
 2    public int majorityElement(int[] nums) {
 3        int n=nums.length;
-4        for(int i=0; i<n; i++){
-5            int count =0;
-6            for(int j=0; j<n; j++){
-7                if (nums[i]==nums[j])
-8                    count++;
-9            }
-10            if (count>Math.floor(n/2))
-11                return nums[i];
-12        }
-13        return -1;
-14    }
-15}
+4        double y=Math.floor(n/2);
+5        for(int i=0; i<n; i++){
+6            int count =0;
+7            for(int j=0; j<n; j++){
+8                if (nums[i]==nums[j])
+9                    count++;
+10            }
+11            if (count>y)
+12                return nums[i];
+13        }
+14        return -1;
+15    }
+16}
