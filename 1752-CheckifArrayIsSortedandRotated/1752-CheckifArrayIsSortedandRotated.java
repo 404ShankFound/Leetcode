f63b583a5c3b33660be077c625091f2300f01624
404ShankFound <shashanknandan21@gmail.com>
2026-01-22 23:56:21 +0530

    File updated at 22/01/2026, 23:56:08

--- 1752-CheckifArrayIsSortedandRotated/1752-CheckifArrayIsSortedandRotated.java
+++ 1752-CheckifArrayIsSortedandRotated/1752-CheckifArrayIsSortedandRotated.java
@@ -1,18 +1,16 @@
-// Last updated: 22/01/2026, 23:55:44
+// Last updated: 22/01/2026, 23:56:08
 1class Solution {
 2    public boolean check(int[] nums) {
-3        int count = 0;
-4        // int n = nums.length;
+3        int n = nums.length;
+4        boolean rotated = false;
 5
-6        for (int i = 0; i < nums.length; i++) {
-7            if (nums[i] > nums[(i + 1) % nums.length]) {
-8                count++;
-9            }
-10            if (count > 1) {
-11                return false;
-12            }
-13        }
-14        return true;
-15    }
-16}
-17
+6        for (int i = 0; i < n; i++) {
+7            if (nums[i] > nums[(i + 1) % n]) {
+8                if (rotated) return false;
+9                rotated = true;
+10            }
+11        }
+12        return true;
+13    }
+14}
+15
