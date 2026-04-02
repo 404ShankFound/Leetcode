5fd1f4c5298961303f52dc9c5a0ce1f572cf977b
404ShankFound <shashanknandan21@gmail.com>
2026-04-02 19:05:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (42.36%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,29 @@
+// class Solution {
+//     public int fib(int n) {
+//         if(n<2){
+//             return n;
+//         }
+//         else{
+//             return fib(n-1)+fib(n-2);
+//         }
+//     }
+// }
+
+class Solution {
+    public int fib(int n) {
+
+        if(n < 2)
+            return n;
+
+        int a = 0;
+        int b = 1;
+
+        for(int i = 2; i <= n; i++){
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return b;
+    }
+}
