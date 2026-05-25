67c2e79210fe1fba0e2cdbffbcd7f838955e88fa
Shashank Nandan <shashanknandan21@gmail.com>
2026-05-25 22:12:34 +0530

    Time: 21 ms (22.75%) | Memory: 53.3 MB (36.47%) - LeetSync

---
+++ 816-design-hashset/design-hashset.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+class Bucket {
+
+    List<Integer> set;
+
+    public Bucket() {
+        set = new LinkedList<>();
+    }
+
+    public void add(int key) {
+        if (!set.contains(key)) {
+            set.add(key);
+        }
+    }
+
+    public void remove(int key) {
+        set.remove(Integer.valueOf(key));
+    }
+
+    public boolean contains(int key) {
+        return set.contains(key);
+    }
+}
+
+class MyHashSet {
+
+    Bucket[] buckets;
+    int size = 111;
+
+    public MyHashSet() {
+        this.buckets = new Bucket[size];
+        for (int i = 0; i < size; i++) {
+            buckets[i] = new Bucket();
+        }
+    }
+    
+    public void add(int key) {
+        int idx = key % 111;
+        buckets[idx].add(key);
+    }
+    
+    public void remove(int key) {
+        int idx = key % 111;
+        buckets[idx].remove(key); 
+    }
+    
+    public boolean contains(int key) {
+        int idx = key % 111;
+        return buckets[idx].contains(key); 
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
