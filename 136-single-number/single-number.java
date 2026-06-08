d32de84a56b122f3897a28baa72883bf65383609
Shashank Nandan <shashanknandan21@gmail.com>
2026-06-08 21:29:54 +0530

    Time: 1 ms (99.96%) | Memory: 47 MB (44.55%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -9,3 +9,26 @@ class Solution {
         return xord;
     }
 }
+
+/* BRUTE FORCE:
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int x : nums){
+            if(map.containsKey(x)){
+                map.put(x, map.get(x) + 1);
+            }
+            else{
+                map.put(x, 1);
+            }
+        }
+        for(int x : nums){
+            if(map.get(x)==1){
+                return x;
+            }
+        }
+        return -1;
+    }
+}
+*/
