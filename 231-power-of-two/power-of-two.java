bc82a0d1237d80b9d78cbe240223efe16e598aba
Shashank Nandan <shashanknandan21@gmail.com>
2026-06-08 21:44:33 +0530

    Time: 1 ms (96.43%) | Memory: 42.4 MB (71.39%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,9 +1,17 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if (n <= 0) return false;
-
-        double x = Math.log(n) / Math.log(2);
-
-        return Math.abs(x - Math.round(x)) < 1e-10;
+        return n>0 && (n & (n - 1)) == 0;
+        // (n>0) FOR Edge cases for 0 = 0.....00 and negative numbers 1.......
     }
 }
+
+/*
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+        double x = Math.log(n) / Math.log(2);
+        return Math.abs(x - Math.round(x)) < 1e-10; 
+        // 1e-10 For edge case of handling Math.log(536870912)/Math.log(2);
+    } 
+}
+*/
