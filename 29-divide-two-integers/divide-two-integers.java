d8e82f73815fcb523cf8a2d852d7e46e77d7e8a6
Shashank Nandan <shashanknandan21@gmail.com>
2026-06-09 00:45:17 +0530

    Time: 1 ms (94.10%) | Memory: 42.7 MB (28.96%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -1,35 +1,62 @@
 class Solution {
     public int divide(int dividend, int divisor) {
 
-        // Overflow case
         if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
         }
-
-        // Determine sign
-        boolean positive =
-                (dividend >= 0 && divisor >= 0) ||
-                (dividend < 0 && divisor < 0);
 
         long dvd = Math.abs((long) dividend);
         long dvs = Math.abs((long) divisor);
 
         int ans = 0;
 
-        while (dvd >= dvs) {
+        for (int i = 31; i >= 0; i--) {
 
-            long temp = dvs;
-            int multiple = 1;
-
-            while ((temp << 1) <= dvd) {
-                temp <<= 1;
-                multiple <<= 1;
+            if ((dvs << i) <= dvd) {
+                dvd -= (dvs << i);
+                ans += (1 << i);
             }
-
-            dvd -= temp;
-            ans += multiple;
         }
 
-        return positive ? ans : -ans;
+        return ((dividend < 0) ^ (divisor < 0))
+                ? -ans
+                : ans;
     }
 }
+// class Solution {
+//     public int divide(int dividend, int divisor) {
+
+//         // Overflow case
+//         if (dividend == Integer.MIN_VALUE && divisor == -1) {
+//             return Integer.MAX_VALUE;
+//         }
+
+//         // Determine sign
+//         boolean positive =
+//                 (dividend >= 0 && divisor >= 0) ||
+//                 (dividend < 0 && divisor < 0);
+
+//         //positive = (divi >= 0) == (d >= 0);
+
+//         long dvd = Math.abs((long) dividend);
+//         long dvs = Math.abs((long) divisor);
+
+//         int ans = 0;
+
+//         while (dvd >= dvs) {
+
+//             long temp = dvs;
+//             int multiple = 1;
+
+//             while ((temp << 1) <= dvd) {
+//                 temp <<= 1;
+//                 multiple <<= 1;
+//             }
+
+//             dvd -= temp;
+//             ans += multiple;
+//         }
+
+//         return positive ? ans : -ans;
+//     }
+// }
