fcb8e83901c7f32af8bffe32f4c50d2a518d1521
Shashank Nandan <shashanknandan21@gmail.com>
2026-06-09 22:08:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (52.73%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,20 +1,36 @@
 //MEMOIZATION USING HELPER FUNCTION
 class Solution {
-
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return solve(n, dp);
+        if (n <= 2) return n;
+
+        long[][] result = {{1, 0}, {0, 1}};
+        long[][] base = {{1, 1}, {1, 0}};
+
+        int power = n;
+
+        while (power > 0) {
+            if ((power & 1) == 1) {
+                result = multiply(result, base);
+            }
+
+            base = multiply(base, base);
+            power >>= 1;
+        }
+
+        return (int) result[0][0];   // Fib(n+1)
     }
 
-    private int solve(int n, int[] dp) {
-        if (n <= 3) {
-            return n;
-        }
-        if (dp[n] != -1) {
-            return dp[n];
-        }
-        return dp[n] = solve(n-1, dp) + solve(n-2, dp);
+    private long[][] multiply(long[][] a, long[][] b) {
+        return new long[][] {
+            {
+                a[0][0] * b[0][0] + a[0][1] * b[1][0],
+                a[0][0] * b[0][1] + a[0][1] * b[1][1]
+            },
+            {
+                a[1][0] * b[0][0] + a[1][1] * b[1][0],
+                a[1][0] * b[0][1] + a[1][1] * b[1][1]
+            }
+        };
     }
 }
 
