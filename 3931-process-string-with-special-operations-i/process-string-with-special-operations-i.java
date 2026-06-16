87cd217d358ccf1d87d3e321b30b47df231c5b9e
Shashank Nandan <shashanknandan21@gmail.com>
2026-06-16 11:58:13 +0530

    Time: 4 ms (76.47%) | Memory: 55 MB (75.00%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder str = new StringBuilder("");
+        int n = s.length(); // Here we take length of original string not intermediate
+        for(int i=0; i<n; i++){
+            char c = s.charAt(i);
+            if('a'<=c && c<='z'){
+                str.append(c);
+            }
+            if(c=='*'){
+                if (str.length() > 0) {  
+                    // otherwise length = 0 so 0-1=-1 StringIndexOutOfBoundsException
+                    str.deleteCharAt(str.length() - 1);
+                }
+            }
+            if(c=='#'){
+                str.append(str);
+            }
+            if(c=='%'){
+                str.reverse();
+            }
+        }
+        s = str.toString();
+        return s;
+    }
+}
