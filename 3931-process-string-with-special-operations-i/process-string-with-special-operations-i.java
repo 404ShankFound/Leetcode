6dce64a090201ad367e3c51c313f1c1e3382767c
Shashank Nandan <shashanknandan21@gmail.com>
2026-06-16 12:02:03 +0530

    Time: 3 ms (100.00%) | Memory: 55.5 MB (36.76%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -1,26 +1,28 @@
 class Solution {
     public String processStr(String s) {
-        StringBuilder str = new StringBuilder("");
+        StringBuilder str = new StringBuilder();
+
         int n = s.length(); // Here we take length of original string not intermediate
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             char c = s.charAt(i);
-            if('a'<=c && c<='z'){
+
+            if ('a' <= c && c <= 'z') {
                 str.append(c);
             }
-            else if(c=='*'){
-                if (str.length() > 0) {  
+            else if (c == '*') {
+                if (str.length() > 0) {
                     // otherwise length = 0 so 0-1=-1 StringIndexOutOfBoundsException
                     str.deleteCharAt(str.length() - 1);
                 }
             }
-            else if(c=='#'){
-                str.append(str);
+            else if (c == '#') {
+                str.append(str.toString());
             }
-            else {
+            else if (c == '%') {
                 str.reverse();
             }
         }
-        s = str.toString();
-        return s;
+
+    return str.toString();
     }
 }
