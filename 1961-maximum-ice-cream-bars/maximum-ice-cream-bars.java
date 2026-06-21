ff5f5e9d0915b7341d38b8f0f5a658fad1d918d6
Shashank Nandan <shashanknandan21@gmail.com>
2026-06-21 07:33:19 +0530

    Time: 36 ms (77.33%) | Memory: 84 MB (28.26%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.java
@@ -3,6 +3,18 @@ class Solution {
         Arrays.sort(costs);
         int i = 0;
         // while (coins > 0 && i < costs.length) { 
+        /*
+        while (coins >= costs[i] && i < costs.length)
+        still has a problem because Java evaluates conditions from left to right.
+        Suppose:
+        costs = [1,2]
+        coins = 10
+        After buying both ice creams:
+        i = 2
+        Now the condition becomes:
+        coins >= costs[2] && 2 < 2
+        But costs[2] does not exist, so the program crashes before even checking 2 < 2
+        */
         while (i < costs.length && coins >= costs[i]) { 
             coins -= costs[i];
             i++;
