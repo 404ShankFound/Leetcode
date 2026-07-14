1d9dc9944ea7a20ce0028b09118729ce32ec1567
Shashank Nandan <shashanknandan21@gmail.com>
2026-07-14 08:55:22 +0530

    Time: 11 ms (73.10%) | Memory: 44.1 MB (39.44%) - LeetSync

--- 1203-print-in-order/print-in-order.java
+++ 1203-print-in-order/print-in-order.java
@@ -1,28 +1,62 @@
 class Foo {
 
-    Semaphore s1 = new Semaphore(0);
-    Semaphore s2 = new Semaphore(0);
+    int turn = 1;
+
     public Foo() {
-        
+
     }
 
-    public void first(Runnable printFirst) throws InterruptedException {
+    public synchronized void first(Runnable printFirst) throws InterruptedException {
         // printFirst.run() outputs "first". Do not change or remove this line.
         printFirst.run();
-        s1.release();
+        turn = 2;
+        notifyAll();
     }
 
-    public void second(Runnable printSecond) throws InterruptedException {
-        s1.acquire();
+    public synchronized void second(Runnable printSecond) throws InterruptedException {
+        while (turn != 2) {
+            wait();
+        }
         // printSecond.run() outputs "second". Do not change or remove this line.
         printSecond.run();
-        s2.release();
+        turn = 3;
+        notifyAll();
     }
 
-    public void third(Runnable printThird) throws InterruptedException {
-        s2.acquire();
+    public synchronized void third(Runnable printThird) throws InterruptedException {
+        while (turn != 3) {
+            wait();
+        }
         // printThird.run() outputs "third". Do not change or remove this line.
         printThird.run();
-        s2.release();
     }
 }
+
+// class Foo {
+
+//     Semaphore s1 = new Semaphore(0);
+//     Semaphore s2 = new Semaphore(0);
+//     public Foo() {
+
+//     }
+
+//     public void first(Runnable printFirst) throws InterruptedException {
+//         // printFirst.run() outputs "first". Do not change or remove this line.
+//         printFirst.run();
+//         s1.release();
+//     }
+
+//     public void second(Runnable printSecond) throws InterruptedException {
+//         s1.acquire();
+//         // printSecond.run() outputs "second". Do not change or remove this line.
+//         printSecond.run();
+//         s2.release();
+//     }
+
+//     public void third(Runnable printThird) throws InterruptedException {
+//         s2.acquire();
+//         // printThird.run() outputs "third". Do not change or remove this line.
+//         printThird.run();
+//         s2.release();
+//     }
+// }
