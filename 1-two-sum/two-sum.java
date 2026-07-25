5832d735b6bca2ae78c65ab454d733ed26ef15c6
Shashank Nandan <shashanknandan21@gmail.com>
2026-07-25 20:22:47 +0530

    Time: 2 ms (99.29%) | Memory: 47.1 MB (40.36%) - LeetSync

---
+++ 1-two-sum/two-sum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int complement=0;
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0; i<nums.length; i++){
+            complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{0,0};
+    }
+}
+// This solution is not valid when any one of the elements has duplicate occurrence
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         int x=0;
+//         Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+//         for(int i=0; i<nums.length; i++){
+//             map.put(nums[i],i);
+//         }
+//         for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+//             x=entry.getKey();
+//             if(map.containsKey(target-x)){
+//                 if()
+//                 return new int[]{entry.getValue(),map.get(target-x)};
+//             }
+//         }
+//         return new int[]{0,0};
+//     }
+// }
