add4b82592958ce10af563a23d8947932a31a8b2
Shashank Nandan <shashanknandan21@gmail.com>
2026-07-28 01:05:11 +0530

    Time: 17 ms (19.68%) | Memory: 47.5 MB (82.51%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        if (k > nums.length) {
+            return new int[]{};
+        }
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            
+            if (map.containsKey(nums[i])) {
+                count = map.get(nums[i])+1;
+                map.put(nums[i], count);
+            }
+            else{
+                map.putIfAbsent(nums[i], 1);
+            }
+        }
+        Queue<Integer> minHeap = new PriorityQueue<Integer>(k, (a, b) -> map.get(a) - map.get(b));
+        for (int num : map.keySet()) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) {
+                minHeap.poll(); // Removes the least frequent element currently in the heap
+            }
+        }
+
+        // 3. Convert the heap into the final result array: O(K log K) time
+        int[] result = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            result[i] = minHeap.poll();
+        }
+
+        return result;
+    }
+}
