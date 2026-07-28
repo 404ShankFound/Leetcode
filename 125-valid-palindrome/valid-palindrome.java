cd64c91c17fda1928f5c0a9ea134bc0ec6437ab6
Shashank Nandan <shashanknandan21@gmail.com>
2026-07-28 18:17:15 +0530

    Time: 2 ms (99.32%) | Memory: 44.4 MB (64.96%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,44 +1,27 @@
 class Solution {
     public boolean isPalindrome(String s) {
-
-        int p1 = 0;
-        int p2 = s.length() - 1;
-
-        while(p1 < p2){
-
-            while(p1 < p2 && !Character.isLetterOrDigit(s.charAt(p1))){
-                p1++;
+        int i = 0;
+        int j = s.length() - 1;
+        
+        while (i < j) {
+            // Move left pointer forward if it's not alphanumeric
+            if (!Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            } 
+            // Move right pointer backward if it's not alphanumeric
+            else if (!Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            } 
+            // Both pointers are at alphanumeric characters; compare them
+            else {
+                // Convert both to lowercase to handle case-insensitivity
+                if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                    return false;
+                }
+                i++;
+                j--;
             }
-
-            while(p1 < p2 && !Character.isLetterOrDigit(s.charAt(p2))){
-                p2--;
-            }
-
-            if(Character.toLowerCase(s.charAt(p1)) != Character.toLowerCase(s.charAt(p2))){
-                return false;
-            }
-
-            p1++;
-            p2--;
         }
-
         return true;
     }
 }
-// class Solution{
-// static boolean isPalindromeRec(String str, int start, int end) {
-//     if (start >= end) {
-//         return true;   // Base condition
-//     }
-
-//     if (str.charAt(start) != str.charAt(end)) {
-//         return false;  // Mismatch condition
-//     }
-
-//     return isPalindromeRec(str, start + 1, end - 1);  // Recursive call
-// }
-// public boolean isPalindrome(String str){
-//     boolean result = isPalindromeRec(str, 0, str.length() - 1);
-//     return result;
-// }
-// }
