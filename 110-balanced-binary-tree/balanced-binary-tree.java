62ab8f7382d2328c73a874e5b612349c9ffbb334
Shashank Nandan <shashanknandan21@gmail.com>
2026-07-30 10:54:28 +0530

    Time: 1 ms (31.54%) | Memory: 45.3 MB (89.62%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -13,19 +13,29 @@
  *     }
  * }
  */
+
 class Solution {
+
     public int maxDepth(TreeNode root) {
         if (root == null) {
             return 0;
         }
+
         return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 
     public boolean isBalanced(TreeNode root) {
+
         if (root == null) {
             return true;
-        } else {
-            return (Math.abs(maxDepth(root.left) - maxDepth(root.right)) <= 1) && isBalanced(root.left) && isBalanced(root.right);
         }
+
+        // Check current node
+        if (Math.abs(maxDepth(root.left) - maxDepth(root.right)) > 1) {
+            return false;
+        }
+
+        // Check left and right subtrees
+        return isBalanced(root.left) && isBalanced(root.right);
     }
 }
