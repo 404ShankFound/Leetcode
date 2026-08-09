c02705262e284bd7e8fc07979bca84b002e40724
Shashank Nandan <shashanknandan21@gmail.com>
2026-08-09 19:11:43 +0530

    Time: 8 ms (6.92%) | Memory: 46.6 MB (33.68%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -13,6 +13,8 @@
  *     }
  * }
  */
+
+ /* O(N*M)
 class Solution {
 
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
@@ -37,5 +39,93 @@ class Solution {
 
         return isIdentical(s.left, t.left) &&
                isIdentical(s.right, t.right);
+    }
+}
+*/
+
+class Solution {
+
+    private void serialize(TreeNode root, StringBuilder sb) {
+
+        if (root == null) {
+            sb.append("# ");
+            return;
+        }
+
+        sb.append(" ").append(root.val).append(" ");
+
+        serialize(root.left, sb);
+        serialize(root.right, sb);
+    }
+
+    private int[] buildLPS(String pattern) {
+
+        int[] lps = new int[pattern.length()];
+
+        int len = 0;
+        int i = 1;
+
+        while (i < pattern.length()) {
+
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else if (len > 0) {
+                len = lps[len - 1];
+            }
+            else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+
+        return lps;
+    }
+
+    private boolean KMP(String text, String pattern) {
+
+        int[] lps = buildLPS(pattern);
+
+        int i = 0;
+        int j = 0;
+
+        while (i < text.length()) {
+
+            if (text.charAt(i) == pattern.charAt(j)) {
+                i++;
+                j++;
+
+                if (j == pattern.length()) {
+                    return true;
+                }
+            }
+            else if (j > 0) {
+                j = lps[j - 1];
+            }
+            else {
+                i++;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+
+        if (subRoot == null)
+            return true;
+
+        if (root == null)
+            return false;
+
+        StringBuilder tree = new StringBuilder();
+        StringBuilder subtree = new StringBuilder();
+
+        serialize(root, tree);
+        serialize(subRoot, subtree);
+
+        return KMP(tree.toString(), subtree.toString());
     }
 }
