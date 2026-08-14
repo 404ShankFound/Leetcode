b2a4c0e467c902c06b3384f02b0ee2b64f4c2915
Shashank Nandan <shashanknandan21@gmail.com>
2026-08-14 23:01:28 +0530

    Time: 1 ms (61.42%) | Memory: 42.6 MB (34.37%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -2,10 +2,18 @@ class Solution {
     public int reverseBits(int n) {
         int ans=0;
         for (int i = 0; i < 32; i++) {
-            int x = n;
-            ans = ans | ((x << (31 - i)) & (1 << (31 - i)));
-            n=n>>1;
+            ans = ans | ((n << (31 - i)) & (1 << (31 - i)));
+            n=n>>>1;
         }
         return ans;
     }
 }
+
+/*
+>> preserves the sign bit.
+For example, if:
+n = 10000000000000000000000000000000
+then:
+n >> 1
+= 11000000000000000000000000000000
+*/
