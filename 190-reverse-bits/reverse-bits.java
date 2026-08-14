842d16a919861192fd565d434bfe3723cdb0abf0
Shashank Nandan <shashanknandan21@gmail.com>
2026-08-14 23:06:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (34.37%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -3,7 +3,8 @@ class Solution {
         int ans = 0;
 
         for (int i = 0; i < 32; i++) {
-            ans = (ans << 1) | (n & 1);
+            ans = (ans << 1);
+            ans |= (n & 1);
             n >>>= 1;
         }
 
