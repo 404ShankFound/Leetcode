57322522a7e1655ac32614137d46843184c14666
Shashank Nandan <shashanknandan21@gmail.com>
2026-09-11 23:06:27 +0530

    Time: 7 ms (9.00%) | Memory: 46.6 MB (86.66%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,32 @@
+/*public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}*/
+public class Solution {
+public boolean hasCycle(ListNode head) {
+    HashSet<ListNode> set = new HashSet<>();
+
+    while(head != null){
+        if(set.contains(head)){
+            return true;
+        }
+
+        set.add(head);
+        head = head.next;
+    }
+
+    return false;
+}}
