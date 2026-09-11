5bd4975a2eb7c1bb82e610f71f0a6d60b97db35c
Shashank Nandan <shashanknandan21@gmail.com>
2026-09-11 21:23:15 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (56.72%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,42 +1,69 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode() {}
+//  *     ListNode(int val) { this.val = val; }
+//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  * }
+//  */
+
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode head = new ListNode(0);
         ListNode curr = head;
-        if (list1 == null) {
-            if (list2 == null) {
-                return null;
-            }
-            return list2;
-        }
-        if (list2 == null) {
-            return list1;
-        }
+
         while (list1 != null && list2 != null) {
             if (list1.val < list2.val) {
-                curr.next = new ListNode(list1.val);
+                curr.next = list1;
                 list1 = list1.next;
             } else {
-                curr.next = new ListNode(list2.val);
+                curr.next = list2;
                 list2 = list2.next;
             }
             curr = curr.next;
+        }
 
-        }
-        if (list1 == null) {
-            curr.next = list2;
-        } else {
+        if (list1 != null) {
             curr.next = list1;
+        } else {
+            curr.next = list2;
         }
+
         return head.next;
     }
 }
+
+// class Solution {
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//         ListNode head = new ListNode(0);
+//         ListNode curr = head;
+//         if (list1 == null) {
+//             if (list2 == null) {
+//                 return null;
+//             }
+//             return list2;
+//         }
+//         if (list2 == null) {
+//             return list1;
+//         }
+//         while (list1 != null && list2 != null) {
+//             if (list1.val < list2.val) {
+//                 curr.next = new ListNode(list1.val);
+//                 list1 = list1.next;
+//             } else {
+//                 curr.next = new ListNode(list2.val);
+//                 list2 = list2.next;
+//             }
+//             curr = curr.next;
+
+//         }
+//         if (list1 == null) {
+//             curr.next = list2;
+//         } else {
+//             curr.next = list1;
+//         }
+//         return head.next;
+//     }
+// }
