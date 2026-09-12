6246af39819b10f46158947a18916e606c45b7cb
Shashank Nandan <shashanknandan21@gmail.com>
2026-09-12 07:42:52 +0530

    Time: 2 ms (86.84%) | Memory: 49 MB (86.58%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -1,47 +1,89 @@
 class Solution {
-    private ListNode rev(ListNode head) {
-        ListNode prev = null;
-
-        while(head != null) {
-            ListNode temp = head.next;
-            head.next = prev;
-            prev = head;
-            head = temp;
-        }
-
-        return prev;
-    }
-
     public void reorderList(ListNode head) {
-        if(head == null || head.next == null) {
+         if (head == null || head.next == null) {
             return;
         }
 
         ListNode slow = head;
         ListNode fast = head;
 
-        while(fast != null && fast.next != null) {
+        while (fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
 
-        ListNode head2 = slow.next;
+        ListNode second = slow.next;
         slow.next = null;
 
-        head2 = rev(head2);
+        ListNode prev = null;
 
-        ListNode curr = head;
-        ListNode temp;
+        while (second != null) {
+            ListNode next = second.next;
+            second.next = prev;
+            prev = second;
+            second = next;
+        }
 
-        while(head2 != null) {
-            temp = curr.next;
-            curr.next = head2;
-            head2 = head2.next;
-            curr.next.next = temp;
-            curr = temp;
+        second = prev;
+
+        ListNode first = head;
+
+        while (second != null) {
+            ListNode firstNext = first.next;
+            ListNode secondNext = second.next;
+
+            first.next = second;
+            second.next = firstNext;
+
+            first = firstNext;
+            second = secondNext;
         }
     }
 }
+// class Solution {
+//     private ListNode rev(ListNode head) {
+//         ListNode prev = null;
+
+//         while(head != null) {
+//             ListNode temp = head.next;
+//             head.next = prev;
+//             prev = head;
+//             head = temp;
+//         }
+
+//         return prev;
+//     }
+
+//     public void reorderList(ListNode head) {
+//         if(head == null || head.next == null) {
+//             return;
+//         }
+
+//         ListNode slow = head;
+//         ListNode fast = head;
+
+//         while(fast != null && fast.next != null) {
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+
+//         ListNode head2 = slow.next;
+//         slow.next = null;
+
+//         head2 = rev(head2);
+
+//         ListNode curr = head;
+//         ListNode temp;
+
+//         while(head2 != null) {
+//             temp = curr.next;
+//             curr.next = head2;
+//             head2 = head2.next;
+//             curr.next.next = temp;
+//             curr = temp;
+//         }
+//     }
+// }
 // class Solution {
 //     private ListNode rev(ListNode head) {
 //         if(head == null || head.next == null) {
