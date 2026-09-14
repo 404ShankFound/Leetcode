8d5a75bbfe8deb52c817616850e754d76eaa1865
Shashank Nandan <shashanknandan21@gmail.com>
2026-09-14 17:55:37 +0530

    Time: 1 ms (99.96%) | Memory: 46.9 MB (29.18%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,23 +9,51 @@
  * }
  */
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
+    private ListNode merge(ListNode a, ListNode b) {
         ListNode head = new ListNode(0);
         ListNode curr = head;
-        PriorityQueue<ListNode> minheap = new PriorityQueue<ListNode>((a, b) -> a.val - b.val);
-        for (int i = 0; i < lists.length; i++) {
-            if (lists[i] != null) {
-                minheap.add(lists[i]);
+
+        while(a != null && b != null) {
+            if(a.val < b.val) {
+                curr.next = a;
+                a = a.next;
+            } else {
+                curr.next = b;
+                b = b.next;
             }
-        }
-        while (!minheap.isEmpty()) {
-            ListNode e = minheap.poll();
-            curr.next = e;
             curr = curr.next;
-            if (e.next != null) {
-                minheap.add(e.next);
-            }
         }
+
+        if(a != null) {
+            curr.next = a;
+        } else {
+            curr.next = b;
+        }
+
         return head.next;
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0) {
+            return null;
+        }
+
+        int n = lists.length;
+
+        while(n > 1) {
+            int j = 0;
+
+            for(int i = 0; i < n / 2; i++) {
+                lists[j++] = merge(lists[i], lists[n - 1 - i]);
+            }
+
+            if(n % 2 != 0) {
+                lists[j++] = lists[n / 2];
+            }
+
+            n = j;
+        }
+
+        return lists[0];
     }
 }
