a504c3dc68052542d93dea502456d42e5c9d090e
Shashank Nandan <shashanknandan21@gmail.com>
2026-09-14 17:57:14 +0530

    Time: 1 ms (99.96%) | Memory: 47.1 MB (14.04%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -23,13 +23,11 @@ class Solution {
             }
             curr = curr.next;
         }
-
         if(a != null) {
             curr.next = a;
         } else {
             curr.next = b;
         }
-
         return head.next;
     }
 
@@ -37,23 +35,42 @@ class Solution {
         if(lists == null || lists.length == 0) {
             return null;
         }
-
         int n = lists.length;
 
         while(n > 1) {
             int j = 0;
-
             for(int i = 0; i < n / 2; i++) {
                 lists[j++] = merge(lists[i], lists[n - 1 - i]);
             }
-
             if(n % 2 != 0) {
                 lists[j++] = lists[n / 2];
             }
-
             n = j;
         }
-
         return lists[0];
     }
 }
+
+/*
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
+        PriorityQueue<ListNode> minheap = new PriorityQueue<ListNode>((a, b) -> a.val - b.val);
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] != null) {
+                minheap.add(lists[i]);
+            }
+        }
+        while (!minheap.isEmpty()) {
+            ListNode e = minheap.poll();
+            curr.next = e;
+            curr = curr.next;
+            if (e.next != null) {
+                minheap.add(e.next);
+            }
+        }
+        return head.next;
+    }
+}
+*/
