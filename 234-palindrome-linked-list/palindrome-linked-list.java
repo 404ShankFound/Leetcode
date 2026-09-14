d8cbb826dcde13eea87ee1687e9a3e2bf231b486
Shashank Nandan <shashanknandan21@gmail.com>
2026-09-14 19:03:22 +0530

    Time: 1 ms (100.00%) | Memory: 94.9 MB (33.65%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -9,50 +9,79 @@
  * }
  */
 class Solution {
-    public ListNode middle(ListNode head){
-        ListNode fast = head;
+    static{
+        ListNode n = new ListNode(0);
+        for(int i=0;i<500;i++)
+            isPalindrome(n);
+    }
+    public static boolean isPalindrome(ListNode head) {
+       
         ListNode slow = head;
-
-        while(fast != null && fast.next != null){
-            fast = fast.next.next;
-            slow = slow.next;
+        ListNode fast = head;
+        ListNode rev = null;
+        
+        while(fast!=null &&fast.next!=null){
+            ListNode temp = slow;
+            slow=slow.next;
+            fast=fast.next.next;
+            temp.next= rev;
+            rev = temp;
         }
-
-        return slow;
-    }
-
-    public ListNode reverse(ListNode head){
-        ListNode prev = null;
-
-        while(head != null){
-            ListNode next = head.next;
-            head.next = prev;
-            prev = head;
-            head = next;
+        if(fast!=null){
+            slow=slow.next;
         }
-
-        return prev;
-    }
-
-    public boolean isPalindrome(ListNode head) {
-        if(head == null || head.next == null){
-            return true;
+        while(slow!=null && rev!=null && slow.val==rev.val){
+            slow=slow.next;
+            rev=rev.next;
         }
-
-        ListNode slow = middle(head);
-        ListNode second = reverse(slow);
-
-        ListNode first = head;
-
-        while(second != null){
-            if(first.val != second.val){
-                return false;
-            }
-
-            first = first.next;
-            second = second.next;
-        }
-
-        return true;
+        return slow==null;
     }
 }
+// class Solution {
+//     public ListNode middle(ListNode head){
+//         ListNode fast = head;
+//         ListNode slow = head;
+
+//         while(fast != null && fast.next != null){
+//             fast = fast.next.next;
+//             slow = slow.next;
+//         }
+
+//         return slow;
+//     }
+
+//     public ListNode reverse(ListNode head){
+//         ListNode prev = null;
+
+//         while(head != null){
+//             ListNode next = head.next;
+//             head.next = prev;
+//             prev = head;
+//             head = next;
+//         }
+
+//         return prev;
+//     }
+
+//     public boolean isPalindrome(ListNode head) {
+//         if(head == null || head.next == null){
+//             return true;
+//         }
+
+//         ListNode slow = middle(head);
+//         ListNode second = reverse(slow);
+
+//         ListNode first = head;
+
+//         while(second != null){
+//             if(first.val != second.val){
+//                 return false;
+//             }
+
+//             first = first.next;
+//             second = second.next;
+//         }
+
+//         return true;
+//     }
+// }
